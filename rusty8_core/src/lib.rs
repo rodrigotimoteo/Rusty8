59fe608c4a49383d2d1a42a45068c858bbfc568f
rodrigotimoteo <rodrigo.timoteo2603@gmail.com>
2024-05-21 23:44:16 +0100

    Added font sprites and reset functionality

--- rusty8_core/src/lib.rs
+++ rusty8_core/src/lib.rs
@@ -1,6 +1,27 @@
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 const START_ADDRESS: u16 = 0x0200;
 
 const RAM_SIZE: usize = 4096;
@@ -23,7 +44,7 @@ pub struct Rusty {
 
 impl Rusty {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu = Self {
             pc: START_ADDRESS,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -34,6 +55,34 @@ impl Rusty {
             keys: [false; NUM_KEYS],
             delay: 0,
             sound: 0,
-        }
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDRESS;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_regs = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.delay = 0;
+        self.sound = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
     }
 }
